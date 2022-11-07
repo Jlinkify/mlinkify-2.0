f48f98ad07f609e992aecf2a95a5c4dabf9bcb25
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2022-11-07 08:12:28 +0000

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const formData = new FormData();
-formData.append('login', 'john.doe');
-formData.append('password', 'johndoe123');
-
-fetch("/api/user/login", {
-    body: formData,
-    method: "post"
-});
